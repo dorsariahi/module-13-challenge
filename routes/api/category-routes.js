ff297603f750f73f7d6d2a63ffa6861d1c876852
dorsariahi <dorsariahiasl@gmail.com>
2023-01-24 12:57:31 -0600

    added all the routes for category model

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -13,25 +13,57 @@ router.get('/', (req, res) => {
   }).catch(
     err => res.json(err)
   )
-  
+
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [Product]
+  }).then(allCategory => {
+    res.json(allCategory)
+  }).catch(
+    err => res.json(err)
+  )
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+    .then((category) => res.json(category))
+    .catch((err) => res.status(400).json(err))
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update({
+    where: {
+      id: req.params.id
+    },
+    include: [Product]
+  }).then(allCategory => {
+    res.json(allCategory)
+  }).catch(
+    err => res.json(err)
+  )
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(allCategory => {
+    res.json(allCategory)
+  }).catch(
+    err => res.json(err)
+  )
 });
 
 module.exports = router;
